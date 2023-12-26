05c4a8d107aeaebf30a2c1c20dde888b72ab5e74
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2023-12-27 02:26:47 +0900

    chore: add dependencies

--- jobcan.el
+++ jobcan.el
@@ -6,10 +6,10 @@
 ;; Keywords: tools
 
 ;; Version: 0.0.0
-;; Package-Requires: ((emacs "25.1") (request "0.3.3") (elquery "1.1.0"))
+;; Package-Requires: ((emacs "25.1") (elquery "1.1.0") (ht "2.4") (request "0.3.3") (s "1.13.1"))
 ;; URL: https://github.com/Kyure-A/jobcan.el
 
-;; SPDX-License-Identifier:  GPL-3.0-or-later
+;; SPDX-License-Identifier: GPL-3.0-or-later
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
