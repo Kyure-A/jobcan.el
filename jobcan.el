9901b0c2ab8dfbaca01aabcd1c9f6c1e70294816
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2024-02-03 01:34:40 +0900

    fix

--- jobcan.el
+++ jobcan.el
@@ -214,10 +214,11 @@
 (defun jobcan-work-time ()
   "Parse the html that can be obtained from LOAD-TOP-INFO (the monthly in load-top-informations)."
   (interactive)
-  (car
-   (mapcar #'jobcan--decode-unicode-escape
-           (reverse (mapcar #'elquery-text
-                     (elquery-$ "span" (elquery-read-string (jobcan--get-top-informations))))))))
+  (message "%s"
+           (car
+            (mapcar #'jobcan--decode-unicode-escape
+                    (reverse (mapcar #'elquery-text
+                                     (elquery-$ "span" (elquery-read-string (jobcan--get-top-informations)))))))))
 
 ;; (jobcan--linked :: (function () ()))
 (defun jobcan-linked ()
